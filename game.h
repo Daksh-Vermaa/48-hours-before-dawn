ce8eee17d17ea1b51acd3c038d9c92d95e791612
Ꭰꭺꮶꮪꮋ Ꮩꭼꭱꮇꭺ <dakshr117@gmail.com>
2026-02-01 20:59:34 +0530

    Define game constants

---
+++ game.h
@@ -0,0 +1,17 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+/*Game Constants*/
+#define NUM_SUSPECTS 4
+#define NUM_LOCATIONS 
+#define MAX_EVIDENCE
+#define SAVE_FILE "detective_save.dat"
+
+
+#endif
