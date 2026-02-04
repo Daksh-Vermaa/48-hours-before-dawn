f20fd219650d9452ce883f65c84a18bdc7c27bbd
Ꭰꭺꮶꮪꮋ Ꮩꭼꭱꮇꭺ <dakshr117@gmail.com>
2026-02-04 15:54:21 +0530

    `Add game state initialization in game.c`

---
+++ crime_scene/game.c
@@ -0,0 +1,17 @@
+#include "game.h"
+#include "suspects/suspects.h"
+#include "evidence/evidence.h"
+
+void init_game_state(GameState *state) {
+    state-> hours_remaining = 48;
+    state->evidence_flags = 0;
+    state->visited_locations = 0;
+    state->current_story_node = NODE_INTRO;
+    state->game_over = 0;
+    state->accusation_made = 0;
+    state->accused_suspect = -1;
+    for (int i = 0; i < NUM_SUSPECTS; i++)
+    {
+        state->suspect_trust[i] = 50; /* Neutral trust */
+    }
+}
