9d25a686b19874a0b5a31ba1074fa2c10ee7b38a
Ꭰꭺꮶꮪꮋ Ꮩꭼꭱꮇꭺ <dakshr117@gmail.com>
2026-02-05 22:05:53 +0530

    `Added location visit functionality`

--- crime_scene/locations/locations.c
+++ crime_scene/locations/locations.c
@@ -1,5 +1,7 @@
 #include "locations.h"
+#include "../ui/ui.h"
 #include "../evidence/evidence.h"
+#include "../suspects/suspects.h"
 #include <string.h>
 
 void init_locations(Location locations[]) {
@@ -45,4 +47,56 @@ void init_locations(Location locations[]) {
     locations[LOC_DETECTIVE_OFFICE].visited = 0;
     locations[LOC_DETECTIVE_OFFICE].times_visited = 0;
     locations[LOC_DETECTIVE_OFFICE].time_cost = 0;
+}
+
+const char* get_location_name(int location_id) {
+    switch(location_id) {
+        case LOC_PM_OFFICE: return "Prime Minister's Office";
+        case LOC_MEDICAL_WING: return "Medical Wing";
+        case LOC_INTEL_ROOM: return "Intelligence Control Room";
+        case LOC_PRESS_AREA: return "Press Area";
+        case LOC_INTERROGATION: return "Interrogation Room";
+        case LOC_DETECTIVE_OFFICE: return "Detective's Office";
+        default: return "Unknown Location";
+    }
+}
+
+void visit_location(GameState *state, Location locations[], Suspect suspects[], int location_id) {
+    Location *loc = &locations[location_id];
+    loc->visited = 1;
+    loc->times_visited++;
+    state->visited_locations |= (1 << location_id);
+    clear_screen();
+    print_header(loc->name, state->hours_remaining);
+    
+    switch(location_id) {
+        case LOC_PM_OFFICE:
+            print_slowly("You return to the Prime Minister's study.", 800);
+            print_slowly("The smell of power lingers. Polished wood. Old paper. Faint tea leaves.", 800);
+            wait_for_space();
+            
+            if (loc->times_visited == 1) {
+                print_slowly("\nOn the desk:", 600);
+                print_slowly("  - A cup of tea, untouched", 600);
+                print_slowly("  - A leather-bound notebook", 600);
+                print_slowly("  - A pen uncapped", 600);
+                wait_for_space();
+                
+                print_slowly("\nYou examine the notebook carefully.", 800);
+                print_slowly("One line catches your attention:", 800);
+                print_slowly("", 400);
+                print_slowly("  'Dawn reshuffle. Some will not forgive me.'", 1000);
+                wait_for_space();
+                
+                collect_evidence(state, EV_NOTEBOOK);
+                collect_evidence(state, EV_RESHUFFLE_PLAN);
+                collect_evidence(state, EV_TEA_CUP);
+            } else {
+                print_slowly("\nNothing new here. The room has already told you its secrets.", 800);
+            }
+            break;
+        
+
+    wait_for_space();
+    deduct_time(state, loc->time_cost);
 }
