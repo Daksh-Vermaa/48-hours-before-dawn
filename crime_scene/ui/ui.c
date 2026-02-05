fa4f64d0639b69450b29f23a818b027470f9c9aa
Ꭰꭺꮶꮪꮋ Ꮩꭼꭱꮇꭺ <dakshr117@gmail.com>
2026-02-05 21:02:39 +0530

    `Added functions for non-canonical input and choice validation`

--- crime_scene/ui/ui.c
+++ crime_scene/ui/ui.c
@@ -50,3 +50,48 @@ void print_typewriter(const char *text, int char_delay_ms) {
     printf("\n");
     fflush(stdout);
 }
+
+char get_single_char(void) {
+    struct termios old_tio, new_tio;
+    char c;
+    tcgetattr(STDIN_FILENO, &old_tio);
+    new_tio = old_tio;
+    new_tio.c_lflag &= (~ICANON & ~ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+    
+    c = getchar();
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+    return c;
+}
+
+void wait_for_space(void) {
+    printf("\n[Press SPACE to continue...]\n");
+    fflush(stdout);
+    char c;
+    do {
+        c = get_single_char();
+    } 
+    while (c != ' ');
+}
+
+int get_choice(int max_options) {
+    int choice;
+    char buffer[256];
+    while (1) {
+        printf("\nYour choice (1-%d): ", max_options);
+        fflush(stdout);
+        
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            continue;
+        }
+        
+        if (sscanf(buffer, "%d", &choice) == 1) {
+            if (choice >= 1 && choice <= max_options) {
+                return choice;
+            }
+        }
+        
+        printf("Invalid choice. Please enter a number between 1 and %d.\n", max_options);
+    }
+}
